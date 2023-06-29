e33170526eafdd16df04fdfd15edc518408f89d9
shaun24 <shauncohen24@gmail.com>
2023-06-29 10:58:08 -0400

    upds

--- layouts/default.vue
+++ layouts/default.vue
@@ -26,14 +26,20 @@ import { useSessionStorage } from '@vueuse/core'
 
 
 useHead(() => ({
-  script: [{
-    innerHTML: `
-      !function(t,n,e,o,a){function d(t){var n=~~(Date.now()/3e5),o=document.createElement(e);o.async=!0,o.src=t+"?ts="+n;var a=document.getElementsByTagName(e)[0];a.parentNode.insertBefore(o,a)}t.MooTrackerObject=a,t[a]=t[a]||function(){return t[a].q?void t[a].q.push(arguments):void(t[a].q=[arguments])},window.attachEvent?window.attachEvent("onload",d.bind(this,o)):window.addEventListener("load",d.bind(this,o),!1)}(window,document,"script","//cdn.stat-track.com/statics/moosend-tracking.min.js","mootrack");
-      mootrack('init', '39463305-b393-4371-b67e-7abe091d8802');
-    `,
-    type: 'text/javascript',
-    charset: 'utf-8'
-  }]
+  script: [
+  {
+      src: 'https://www.googletagmanager.com/gtag/js?id=G-WNFHNN3RX6',
+      async: true,
+    },
+    {
+      innerHTML: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-WNFHNN3RX6');
+      `,
+    },
+  ]
 }))
 
 
